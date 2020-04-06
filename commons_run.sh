97f23a7e5d54b290f94026deb3dea4950ffd1864
houweiqiang <2639252035Hwq>
2020-04-06 18:02:31 +0800

    测试

--- commons_run.sh
+++ commons_run.sh
@@ -66,8 +66,7 @@ function start() {
     fi
 
     nohup java $OTHER_OPTS $NEW_RELIC_OPT -jar $NAME.jar >> log 2>&1 &
-    x=pwd
-    echo $x
+    echo "$(pwd)"
     echo "nohup java $OTHER_OPTS $NEW_RELIC_OPT -jar $NAME.jar"
     new_pid=$!
     echo $new_pid > RUNNING_PID
