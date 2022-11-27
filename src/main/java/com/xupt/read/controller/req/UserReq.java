446f2f54217250340fe35e3f41b3ae2ac2a9df76
hwq <x2639252035@163.com>
2022-11-27 18:06:52 +0800

    用户登录注册设置创建更新时间

--- src/main/java/com/xupt/read/controller/req/UserReq.java
+++ src/main/java/com/xupt/read/controller/req/UserReq.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 /**
  * 用户请求实体类
@@ -21,6 +22,8 @@ public class UserReq {
     public static User convert(UserReq req) {
         User user = new User();
         BeanUtils.copyProperties(req, user);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
         return user;
     }
 }
